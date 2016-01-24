03ab0dfeb7fb322ee1b4780f8e630f398c362a35
horance <horance@outlook.com>
2016-01-24 14:09:46 +0800

    refactorint to java8

--- java/src/main/java/fizz/buzz/whizz/Rules.java
+++ java/src/main/java/fizz/buzz/whizz/Rules.java
@@ -1,28 +1,29 @@
 package fizz.buzz.whizz;
 
+import java.util.Arrays;
+
 public final class Rules {
   public static Rule atom(Matcher matcher, Action action) {
     return (n, rr) -> rr.collect(matcher.matches(n), action.to(n));
   }
 
-  private static Rule combine(boolean shortcut, Rule... rules) {
-    return (n, rr) -> {
-      for (Rule r : rules)
-        if (r.apply(n, rr) == shortcut)
-          return shortcut;
-      return !shortcut;
-    };
-  }
-
   public static Rule anyof(Rule... rules) {
-    return combine(true, rules);
+    return (n, rr) -> Arrays.asList(rules)
+        .stream()
+        .anyMatch(rule -> rule.apply(n, rr));
   }
  
   public static Rule allof(Rule... rules) {
     return (n, rr) -> {
       RuleResult result = new RuleResult();
-      return rr.collect(combine(false, rules).apply(n, result), result);
+      return rr.collect(allMatch(rules).apply(n, result), result);
     };
+  }
+  
+  private static Rule allMatch(Rule... rules) {
+    return (n, rr) -> Arrays.asList(rules)
+        .stream()
+        .allMatch(rule -> rule.apply(n, rr));
   }
   
   private Rules() {
