d25822d963e089615449bc350ba704d9c6f23e50
horance <horance@aliyun.com>
2017-10-15 09:47:37 +0800

    Update Rule.cpp

--- cpp/src/fizz-buzz-whizz/Rule.cpp
+++ cpp/src/fizz-buzz-whizz/Rule.cpp
@@ -1,27 +1,27 @@
 #include "fizz-buzz-whizz/Rule.h"
-#include <numeric>
-#include <algorithm>
 
 Rule atom(Matcher&& matcher, Action&& action) {
-  return [ matcher = std::move(matcher) 
-         , action = std::move(action)](auto n) {
-    return matcher(n) ? action(n) : "";
-  };
-}
-
-Rule anyof(std::vector<Rule>&& rules) {
-  return [rules = std::move(rules)](auto n) {
-    auto found = std::find_if(rules.cbegin(), rules.cend(),
-      [n](const auto& r) { return !r(n).empty(); });
-    return found != std::cend(rules) ? (*found)(n) : "";
+  return [matcher = std::move(matcher), action = std::move(action)](int m) {
+    return matcher(m) ? action(m) : "";
   };
 }
 
 Rule allof(std::vector<Rule>&& rules) {
-  return [rules = std::move(rules)](auto n) {
-    return std::accumulate(rules.cbegin(), rules.cend(), std::string(""),
-      [n](const auto& acc, const auto& r) {
-        return acc + r(n);
-    });
+  return [rules = std::move(rules)](int m) {
+    std::string result;
+    for (auto& rule : rules)
+      result += rule(m);
+    return result;
+  };
+}
+
+Rule anyof(std::vector<Rule>&& rules) {
+  return [rules = std::move(rules)](int m) {
+    for (auto& rule : rules) {
+      auto result = rule(m);
+      if (!result.empty())
+        return result;
+    }
+    return std::string("");
   };
 }
